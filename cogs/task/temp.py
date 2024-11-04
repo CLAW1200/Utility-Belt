dc2c139b6f1fe34d67f28a608869b915aa585f75
claw1200 <carstenwood12@gmail.com>
2024-11-04 17:11:19 +0000

    removed some questionable /tmp/ cleanup code

--- cogs/task/temp.py
+++ cogs/task/temp.py
@@ -15,15 +15,6 @@ class CleanTemp(Cog):
     @tasks.loop(seconds=(300))
     async def cleaner(self):
         now = datetime.datetime.now()
-        # if a file or directory in temp/ is older than 5 minutes, delete it
-        for file in os.listdir("temp/"):
-            file_path = os.path.join("temp/", file)
-            file_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
-            if (now - file_time).seconds > 300:
-                if os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-                else:
-                    os.remove(file_path)
         for file in os.listdir("/tmp/"):
             if file.startswith("utilitybelt_"):
                 file_path = os.path.join("/tmp/", file)
